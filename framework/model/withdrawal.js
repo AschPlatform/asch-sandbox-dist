39a632871ea8fdc3ce3843887c4dadf4f71991a0
sqfasd <sqf1225@foxmail.com>
2018-06-08 18:39:35 +0800

    before asch mainchain 1.4.0-beta

---
+++ framework/model/withdrawal.js
@@ -0,0 +1,1 @@
+"use strict";module.exports={table:"withdrawals",tableFields:[{name:"tid",type:"String",length:64,primary_key:!0},{name:"recipientId",type:"String",length:64},{name:"seq",type:"Number",index:!0},{name:"currency",type:"String",length:30,not_null:!0,index:!0},{name:"amount",type:"String",length:50,not_null:!0}]};
